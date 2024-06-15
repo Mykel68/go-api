577e07ad408d99267f05a37d8d6cd34a14a797aa
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-15 23:30:15 +0100

    return book

--- main.go
+++ main.go
@@ -70,6 +70,22 @@ func checkoutBook (c *gin.Context){
 	c.IndentedJSON(http.StatusOK, book)
 }
 
+func returnbook(c *gin.Context){
+	id, ok := c.GetQuery(("id"))
+	if !ok {	
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+	book, err := getBookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"Book not found"})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func createBook(c *gin.Context){
 	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
@@ -85,6 +101,7 @@ func main(){
 	router.GET("/books", getbooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
-	router.PATCH("/books/:id", checkoutBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnbook)
 	router.Run("localhost:8080")
 }
