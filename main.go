ce6169df4219a9c93f679b6db79aa4ea1cc1f183
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-15 22:42:42 +0100

    pushing

--- main.go
+++ main.go
@@ -14,7 +14,54 @@ type book struct {
 	Quantity int `json:"quantity"`
 }
 
-var books []book{
+var books = []book{
 	{ID: "1", Title: "Book 1", Author: "Author 1", Quantity: 10},
 	{ID: "2", Title: "Book 2", Author: "Author 2", Quantity: 5},
-	{ID: "3", Title: "Book 3", Author: "Author 3", Quantity: 20},}
+	{ID: "3", Title: "Book 3", Author: "Author 3", Quantity: 20},
+
+
+}
+
+func getbooks(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, books)  
+}
+
+
+
+func bookById (c *gin.Context) {
+	id := c.Param("id")
+
+	book, err := getBookById(id)
+	if err != nil {	
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+
+}
+
+func getBookById(id string) (*book, error){
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+func createBook(c *gin.Context){
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+
+}
+
+func main(){
+	router := gin.Default()
+	router.GET("/books", getbooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.Run("localhost:8080")
+}
